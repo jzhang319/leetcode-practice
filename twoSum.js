291bbd05573ec2fc2efd1369682609536a941fae
jack <jzhang319@gmail.com>
2023-04-05 20:30:33 -0400

    finished 2sum

--- twoSum.js
+++ twoSum.js
@@ -1,19 +1,19 @@
 // let nums = [2, 7, 11, 15];
 // let target = 9;
 
-let nums = [3, 2, 4]
-let target = 6
+let nums = [3, 2, 4];
+let target = 6;
 
 const twoSum = function (nums, target) {
   let ansArr = [];
-  for (let i = 1; i < nums.length; i++) {
-    if ((nums[i] + nums[i - 1]) === target) {
-      ansArr.push(i - 1);
-      ansArr.push(i);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        ansArr.push(i, j);
+      }
     }
   }
   return ansArr;
 };
-
 
 console.log(twoSum(nums, target));
