ec51cf186128e322388805cc61e7d4502f4b4f71
jack <jzhang319@gmail.com>
2023-07-13 12:11:30 -0400

    still working on linked list

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -1,6 +1,32 @@
-let l1 = [2,4,3]
-let l2 = [5,6,4]
-
-const addTwoNumbers = (l1, l2) => {
-
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
 }
+class SinglyLinkedList {
+    constructor(Head = null) {
+        this.Head = Head
+    }
+
+add(newNode){
+    let node = this.Head;
+    if(node==null){
+        this.Head = newNode;
+        return;
+    }
+    while (node.next) {
+        node = node.next;
+    }
+    node.next = newNode;
+}
+
+// let l1 = [2,4,3]
+// let l2 = [5,6,4]
+
+// const addTwoNumbers = (l1, l2) => {
+
+// }
+
+
+// addTwoNumbers(l1, l2)
