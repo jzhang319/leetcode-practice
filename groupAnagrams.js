0299b59f0802406cbbd8cce6e123a2a2b9c9f3e0
jack <jzhang319@gmail.com>
2023-07-28 16:38:39 -0400

    double checked

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -14,14 +14,14 @@
 let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 
 const groupAnagrams = function (strs) {
-  let sorted = strs.split("").sort().join("");
+  let sorted = strs.map((strs) => strs.split("").sort().join(""));
   let hash = {};
 
   for (let i = 0; i < sorted.length; i++) {
     if (!hash[sorted[i]]) {
-      hash[sorted[i]] = [sorted[i]];
+      hash[sorted[i]] = [strs[i]];
     } else {
-      hash[sorted[i]].push(sorted[i]);
+      hash[sorted[i]].push(strs[i]);
     }
   }
   return Object.values(hash);
