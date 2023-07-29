458dbb0cde036f05f08c23636633f00152d0ae7f
jack <jzhang319@gmail.com>
2023-07-29 16:45:10 -0400

    solved 005

--- 005topKFrequentElements.js
+++ 005topKFrequentElements.js
@@ -1,32 +1,38 @@
 // Input: nums = [1,1,1,2,2,3], k = 2
 // Output: [1,2]
 
-let nums = [1, 1, 1, 2, 2, 3]
-let k = 2
+let nums = [1, 1, 1, 2, 2, 3];
+let k = 2;
 
 const topKFrequent = (nums, k) => {
-  let hash = {}
+  let hash = {};
   for (let i = 0; i < nums.length; i++) {
-    let currNum = nums[i]
+    let currNum = nums[i];
     if (hash[currNum]) {
-          hash[currNum]++
-        } else {
-          hash[currNum] = 1
-        }
+      hash[currNum]++;
+    } else {
+      hash[currNum] = 1;
+    }
   }
   // console.log(hash)
 
-  let arr = []
-  for (let key in hash) {
-    let count = hash[key]
-    // console.log(count)
-    // console.log(key)
-    if (hash[key] >= k) {
-      arr.push(key)
-    }
-  }
+  // let arr = []
+  // for (let key in hash) {
+  //   let count = hash[key]
+  //   // console.log(count)
+  //   // console.log(key)
+  //   if (count >= k) {
+  //     arr.push(key)
+  //   }
+  // }
   // console.log(arr)
-  return arr
+  // return arr
+
+  let sortedArr = [...Object.keys(hash)]
+    .sort((a, b) => hash[b] - hash[a])
+    .slice(0, k);
+  console.log(sortedArr);
+  return sortedArr;
 };
 
-topKFrequent(nums, k)
+topKFrequent(nums, k);
