aff561f70d5b922242d186f4429a4a8e81606bc4
jack <jzhang319@gmail.com>
2023-08-02 13:23:08 -0400

    fixed set error

--- 007validSudoku.js
+++ 007validSudoku.js
@@ -26,6 +26,7 @@ var isValidSudoku = function (board) {
     for (let j = 0; j < 3; j++) {
 
       // sub board or mini boards
+      const set = new Set();
       for (let k = 0; k < 3; k++) {
         for (let l = 0; l < 3; l++) {
           const cell = board[3 * i + k][3 * j + l];
