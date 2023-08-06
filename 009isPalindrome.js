db8b5b4ae9c8b16f337078fc30cd6ba83b91c83b
jack <jzhang319@gmail.com>
2023-08-06 16:23:16 -0400

    fixed is palingdrome

--- 009isPalindrome.js
+++ 009isPalindrome.js
@@ -1,5 +1,24 @@
 var isPalindrome = function (s) {
-  let reversed = x.toString().split("").reverse().join("");
-  //  console.log(typeof(reversed));
-  return s.toString() === reversed;
+  // 1) clean string , replace all non-alphanumeric characters with empty string
+  // 2) lowercase the string
+  // 3) check
+
+  const check = (s) => {
+    let l = 0;
+    let r = s.length - 1;
+    while (l <= r) {
+      if (s[l] === s[r]) {
+        l++;
+        r--;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const regex = /[^a-zA-Z0-9]/g;
+  const clean = s.replace(regex, "").toLowerCase();
+  // console.log(clean);
+  return check(clean);
 };
