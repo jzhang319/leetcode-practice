4d1720936ba1f299445528fe3cd726a75fa19bb3
jzhang319 <jzhang319@gmail.com>
2023-08-10 14:45:59 -0400

    worked for first set

--- 011threeSum.js
+++ 011threeSum.js
@@ -1,18 +1,23 @@
 let nums = [-1, 0, 1, 2, -1, -4];
 // output: [[-1,-1,2], [-1,0,1]]
 
-var threeSum = function(nums) {
+var threeSum = function (nums) {
   if (nums.length < 3 || nums === null) return [];
 
   let sorted = nums.sort((a, b) => a - b);
   // console.log(sorted);
-  let l = i + 1;
-  let r = nums.length - 1;
+
+  let hash = {};
 
   for (let i = 0; i < nums.length - 2; i++) {
-    
+    // let l = i + 1;
+    // let r = nums.length - 1;
+    // console.log(sorted[i], sorted[i + 1], sorted[i + 2]);
+    if (sorted[i] + sorted[i + 1] + sorted[i + 2] === 0) {
+      hash[[sorted[i], sorted[i + 1], sorted[i + 2]]];
+    }
   }
+  console.log(hash);
 };
 
-
-console.log(threeSum(nums))
+console.log(threeSum(nums));
