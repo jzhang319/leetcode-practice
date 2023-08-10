5ccc052150a2bc8e55d0b1789963e88713742992
jzhang319 <jzhang319@gmail.com>
2023-08-10 15:33:53 -0400

    finished 3sum

--- 011threeSum.js
+++ 011threeSum.js
@@ -15,10 +15,21 @@ var threeSum = function (nums) {
     let j = i + 1;
     let k = nums.length - 1;
     // console.log(sorted[i], sorted[i + 1], sorted[i + 2]);
-    if (nums[i] + nums[j] + nums[k] === 0) {
-      ans.push([nums[i], nums[i + 1], nums[i + 2]]);
+    while (j < k) {
+      let sum = nums[i] + nums[j] + nums[k];
+      if (sum === 0) {
+        ans.push([nums[i], nums[j], nums[k]]);
+        // stopping duplicates
+        while (nums[j] === nums[j + 1]) j++;
+        while (nums[k] === nums[k + 1]) k--;
+        j++;
+        k--;
+      } else if (sum < 0) {
+        j++;
+      } else {
+        k--;
+      }
     }
-    
   }
   // console.log(ans);
   return ans;
