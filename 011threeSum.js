af9517b2ca1f4ff612ecacf15938f5a2a7b90acb
jzhang319 <jzhang319@gmail.com>
2023-08-10 15:07:31 -0400

    try combining with 2 sum method

--- 011threeSum.js
+++ 011threeSum.js
@@ -4,20 +4,24 @@ let nums = [-1, 0, 1, 2, -1, -4];
 var threeSum = function (nums) {
   if (nums.length < 3 || nums === null) return [];
 
-  let sorted = nums.sort((a, b) => a - b);
+  nums = nums.sort((a, b) => a - b);
   // console.log(sorted);
 
-  let hash = {};
+  let ans = [];
 
   for (let i = 0; i < nums.length - 2; i++) {
-    // let l = i + 1;
-    // let r = nums.length - 1;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let j = i + 1;
+    let k = nums.length - 1;
     // console.log(sorted[i], sorted[i + 1], sorted[i + 2]);
-    if (sorted[i] + sorted[i + 1] + sorted[i + 2] === 0) {
-      hash[[sorted[i], sorted[i + 1], sorted[i + 2]]];
+    if (nums[i] + nums[j] + nums[k] === 0) {
+      ans.push([nums[i], nums[i + 1], nums[i + 2]]);
     }
+    
   }
-  console.log(hash);
+  // console.log(ans);
+  return ans;
 };
 
 console.log(threeSum(nums));
