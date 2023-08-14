7d7294f830a6ae326329f4bc52f01c5587d24bd2
jack <jzhang319@gmail.com>
2023-08-13 22:06:43 -0400

    finished length of longest substring

---
+++ 015longtestSubstringNoRepeating.js
@@ -0,0 +1,19 @@
+var lengthOfLongestSubstring = function (s) {
+  let map = {};
+  let maxLength = 0;
+  let start = 0;
+  for (let i = 0; i < s.length; i++) {
+    let lastChar = s[i];
+    if (map[lastChar] === undefined) {
+      map[lastChar] = 0;
+    }
+    map[lastChar]++;
+    while (map[lastChar] > 1) {
+      let firstChar = s[start];
+      map[firstChar]--;
+      start += 1;
+    }
+    maxLength = Math.max(maxLength, i - start + 1);
+  }
+  return maxLength;
+};
