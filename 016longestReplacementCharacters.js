613c99b4b0fb0215ccbbbad2066374261c9d57e8
jack <jzhang319@gmail.com>
2023-08-21 13:07:38 -0400

    not working

--- 016longestReplacementCharacters.js
+++ 016longestReplacementCharacters.js
@@ -1,3 +1,25 @@
-var characterReplacement = function(s, k) {
-  
+var characterReplacement = function (s, k) {
+  let windowStart = 0;
+  let maxLength = 0;
+  let mostCommon = 0;
+  let hash = {};
+
+  for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+    if (!hash[s[windowEnd]]) {
+      hash[s[windowEnd]] = 0;
+    }
+    hash[s[windowEnd]]++;
+
+    mostCommon = Math.max(mostCommon, hash[s[windowEnd]]);
+
+    // temp length = windowEnd - windowStart + 1
+
+    if (windowEnd - windowStart + 1 - mostCommon) {
+      // condition where the window is no longer valid
+      hash[s[windowStart]]--;
+      windowStart++;
+    }
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+  return maxLength;
 };
