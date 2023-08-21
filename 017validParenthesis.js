03413e9f53c147bf06d403412e6dbfd20b505823
jack <jzhang319@gmail.com>
2023-08-21 14:44:20 -0400

    general testing

--- 017validParenthesis.js
+++ 017validParenthesis.js
@@ -1,3 +1,20 @@
-var isValid = function(s) {
+let s = "()[]{}";
 
+var isValid = function (s) {
+  // check if the string is empty
+  if (s.length === 0) {
+    return false;
+  }
+  // split the string
+  let splitArr = s.split("");
+
+  // check if each set of parenthesis is followed by a paired parenthesis
+  for (let i = 0; i < splitArr.length - 1; i++) {
+    let curr = splitArr[i];
+    console.log(curr);
+    // if (splitArr[i] = '(')
+  }
+  return false
 };
+
+console.log(isValid(s));
