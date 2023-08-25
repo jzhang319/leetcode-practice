a0fba58d57a355b3b440e5b20362a51477ec131c
jack <jzhang319@gmail.com>
2023-08-25 11:43:07 -0400

    finished remove duplicates from sorted arrays

---
+++ 019removeDuplicatesFromSortedArray.js
@@ -0,0 +1,12 @@
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+var removeDuplicates = function (nums) {
+  let index = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[index] = nums[i + 1];
+      index++;
+    }
+  }
+  return index;
+};
