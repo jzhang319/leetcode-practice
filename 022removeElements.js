921e18ebb89515399796581e767dcf90e81b0f45
jack <jzhang319@gmail.com>
2023-08-29 14:47:30 -0400

    solved 22

--- 022removeElements.js
+++ 022removeElements.js
@@ -1,7 +1,17 @@
-let nums = [3, 2, 2, 3]
-let val = 3
+let nums = [3, 2, 2, 3];
+let val = 3;
 // Output: 2, (nums = [2, 2, _, _]);
 
 var removeElement = function (nums, val) {
-  
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      // console.log(nums[i]);
+      nums[count] = nums[i];
+      count++;
+    }
+  }
+  return count;
 };
+
+console.log(removeElement(nums, val));
