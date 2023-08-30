b2532a2542b3a2b2cee1f4ec4938a7e0de527a51
jack <jzhang319@gmail.com>
2023-08-30 11:09:58 -0400

    finished climbing stairs

--- 023climbingStairs.js
+++ 023climbingStairs.js
@@ -1,5 +1,5 @@
 // example 1
-let n = 2
+let n = 2;
 // Output: 2
 // Explanation: There are two ways to climb to the top.
 // 1. 1 step + 1 step
@@ -13,13 +13,15 @@ let n = 2
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
 
-var climbStairs = function(n) {
-  for (let i = 0; i <= n; i++) {
-    let current = i
-    console.log(current);
+var climbStairs = function (n) {
+  let counts = [];
+  counts[1] = 1;
+  counts[2] = 2;
 
+  for (let i = 3; i <= n; i++) {
+    counts[i] = counts[i - 1] + counts[i - 2];
   }
+  return counts[n];
 };
-
 
 console.log(climbStairs(n));
