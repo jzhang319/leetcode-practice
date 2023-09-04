a80af486e36005c7ee19020e75de7c34c07d192c
jack <jzhang319@gmail.com>
2023-09-03 21:08:00 -0400

    finished 024

--- 024firstOccurString.js
+++ 024firstOccurString.js
@@ -1,6 +1,6 @@
 // Example 1:
-let haystack = "sadbutsad"
-let needle = "sad"
+let haystack = "sadbutsad";
+let needle = "sad";
 // Output: 0
 // Explanation: "sad" occurs at index 0 and 6.
 // The first occurrence is at index 0, so we return 0.
@@ -10,9 +10,26 @@ let needle = "sad"
 // Output: -1
 // Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
-
-
-var strStr = function(haystack, needle) {
+var strStr = function (haystack, needle) {
   if (needle.length > haystack.length) return -1;
-  
+  let i = 0,
+    j = 0,
+    k = 0;
+
+  while (i < haystack.length) {
+    while (needle[j] == haystack[k]) {
+      j++;
+      k++;
+      if (j == needle.length) return i;
+      if (k == haystack.length) return -1;
+    }
+    if (needle[j] != haystack[k]) {
+      i++;
+      j = 0;
+      k = i;
+    }
+  }
+  return -1;
 };
+
+console.log(strStr(haystack, needle));
