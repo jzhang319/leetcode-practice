0fcf49c9584b03ce72d79eebbd2682b14627e367
jack <jzhang319@gmail.com>
2023-09-03 20:57:14 -0400

    working

---
+++ 024firstOccurString.js
@@ -0,0 +1,18 @@
+// Example 1:
+let haystack = "sadbutsad"
+let needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+
+// Example 2:
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+
+var strStr = function(haystack, needle) {
+  if (needle.length > haystack.length) return -1;
+  
+};
