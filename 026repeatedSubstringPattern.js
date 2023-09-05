cdfc41f24fd8d118549f9ab01d9dc2731e4cd547
jack <jzhang319@gmail.com>
2023-09-05 18:59:14 -0400

    working

--- 026repeatedSubstringPattern.js
+++ 026repeatedSubstringPattern.js
@@ -1,3 +1,16 @@
-var repeatedSubstringPattern = function(s) {
+// Example 1:
 
-};
+// Input: s = "abab"
+// Output: true
+// Explanation: It is the substring "ab" twice.
+// Example 2:
+
+// Input: s = "aba"
+// Output: false
+// Example 3:
+
+// Input: s = "abcabcabcabc"
+// Output: true
+// Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+
+var repeatedSubstringPattern = function (s) {};
