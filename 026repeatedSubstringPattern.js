d7c95191ebea96dbf28f88aa264c585c556f78c7
jack <jzhang319@gmail.com>
2023-09-07 16:00:47 -0400

    working

--- 026repeatedSubstringPattern.js
+++ 026repeatedSubstringPattern.js
@@ -1,11 +1,11 @@
 // Example 1:
 
-// Input: s = "abab"
+// let s = "abab"
 // Output: true
 // Explanation: It is the substring "ab" twice.
 // Example 2:
 
-// Input: s = "aba"
+let s = "aba"
 // Output: false
 // Example 3:
 
@@ -14,5 +14,16 @@
 // Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
 
 var repeatedSubstringPattern = function (s) {
+  if (s.length === 0) return false;
   
+  let prefix = s[0];
+  for (let i = 1; i < s.length; i++) {
+    while (s[i].indexOf(prefix)!== 0) {
+      prefix = prefix.slice(0, -1)
+    }
+  }
+  return true;
 };
+
+
+console.log(repeatedSubstringPattern(s))
