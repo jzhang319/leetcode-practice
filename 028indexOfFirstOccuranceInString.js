bc8008b0b65d1a271bb056b0eb4ecb4099c64685
jack <jzhang319@gmail.com>
2023-09-07 16:32:14 -0400

    starting 028

---
+++ 028indexOfFirstOccuranceInString.js
@@ -0,0 +1,16 @@
+// Example 1:
+
+let haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// let haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation:
+
+
+var strStr = function(haystack, needle) {
+
+};
