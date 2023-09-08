fc100ed51eb4031350ea220bc1d5147fa9094662
jack <jzhang319@gmail.com>
2023-09-08 11:01:42 -0400

    finished 028, same as first occurance of string

--- 028indexOfFirstOccuranceInString.js
+++ 028indexOfFirstOccuranceInString.js
@@ -1,6 +1,7 @@
 // Example 1:
 
-let haystack = "sadbutsad", needle = "sad"
+let haystack = "sadbutsad",
+  needle = "sad";
 // Output: 0
 // Explanation: "sad" occurs at index 0 and 6.
 // The first occurrence is at index 0, so we return 0.
@@ -10,7 +11,26 @@ let haystack = "sadbutsad", needle = "sad"
 // Output: -1
 // Explanation:
 
+var strStr = function (haystack, needle) {
+  if (needle.length > haystack.length) return -1;
+  let i = 0,
+    j = 0,
+    k = 0;
 
-var strStr = function(haystack, needle) {
-
+  while (i < haystack.length) {
+    while (needle[j] == haystack[k]) {
+      j++;
+      k++;
+      if (j == needle.length) return i;
+      if (k == haystack.length) return -1;
+    }
+    if (needle[j] != haystack[k]) {
+      i++;
+      j = 0;
+      k = i;
+    }
+  }
+  return -1;
 };
+
+console.log(strStr(haystack, needle));
