8e44d8638a30f50ac9ea0996a8999c9cdf26753b
jack <jzhang319@gmail.com>
2023-09-08 11:09:53 -0400

    first try

--- 029mergeTwoSortedLists.js
+++ 029mergeTwoSortedLists.js
@@ -1,5 +1,6 @@
 // Example 1:
-// Input: list1 = [1,2,4], list2 = [1,3,4]
+let list1 = [1, 2, 4],
+  list2 = [1, 3, 4];
 // Output: [1,1,2,3,4,4]
 // Example 2:
 
@@ -10,6 +11,28 @@
 // Input: list1 = [], list2 = [0]
 // Output: [0]
 
-var mergeTwoLists = function(list1, list2) {
-
+var mergeTwoLists = function (list1, list2) {
+  if (!list1) return list2;
+  if (!list2) return list1;
+  let result = [];
+  let i = 0,
+    j = 0;
+  while (i < list1.length || j < list2.length) {
+    if (i === list1.length) {
+      result.push(list2[j]);
+      j++;
+    } else if (j === list2.length) {
+      result.push(list1[i]);
+      i++;
+    } else if (list1[i] < list2[j]) {
+      result.push(list1[i]);
+      i++;
+    } else {
+      result.push(list2[j]);
+      j++;
+    }
+  }
+  return result;
 };
+
+console.log(mergeTwoLists(list1, list2));
