46ec5ee755b42246f43f4edc4a46b6cb60fa4ecf
jack <jzhang319@gmail.com>
2023-09-08 11:50:19 -0400

    starting 030

---
+++ 030countingBits.js
@@ -0,0 +1,23 @@
+// Example 1:
+
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// Example 2:
+
+// Input: n = 5
+// Output: [0,1,1,2,1,2]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+
+var countBits = function(n) {
+
+};
