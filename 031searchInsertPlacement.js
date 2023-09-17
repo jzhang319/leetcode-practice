762eac58ff6ecbc6b851c68470c6747a88d0ef00
jzhang319 <jzhang319@gmail.com>
2023-09-17 15:35:43 -0400

    added search insert position

---
+++ 031searchInsertPlacement.js
@@ -0,0 +1,20 @@
+// Example 1:
+
+// let nums = [1, 3, 5, 6],
+//   target = 5;
+// Output: 2
+
+// Example 2:
+
+let nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// let nums = [1,3,5,6], target = 7
+// Output: 4
+
+var searchInsert = function (nums, target) {
+
+};
+
+console.log(searchInsert(nums, target));
