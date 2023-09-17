415613cbbf953b90a2bffaa712848d2e66ef9cd2
jzhang319 <jzhang319@gmail.com>
2023-09-17 15:36:48 -0400

    finished 031

--- 031searchInsertPlacement.js
+++ 031searchInsertPlacement.js
@@ -14,7 +14,19 @@ let nums = [1,3,5,6], target = 2
 // Output: 4
 
 var searchInsert = function (nums, target) {
-
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    let middle = Math.trunc((left + right) / 2);
+    if (nums[middle] === target) {
+      return middle;
+    } else if (nums[middle] < target) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return left;
 };
 
 console.log(searchInsert(nums, target));
