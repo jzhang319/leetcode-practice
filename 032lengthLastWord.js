7dc7460da53b5307260ce60283315ac46b01d22a
jzhang319 <jzhang319@gmail.com>
2023-09-17 15:53:54 -0400

    finished 032

--- 032lengthLastWord.js
+++ 032lengthLastWord.js
@@ -1,19 +1,29 @@
 // Example 1:
 
-// Input: s = "Hello World"
+let s = "Hello World   "
 // Output: 5
 // Explanation: The last word is "World" with length 5.
+
 // Example 2:
 
-// Input: s = "   fly me   to   the moon  "
+// let s = "   fly me   to   the moon  "
 // Output: 4
 // Explanation: The last word is "moon" with length 4.
+
 // Example 3:
 
-// Input: s = "luffy is still joyboy"
+// let s = "luffy is still joyboy"
 // Output: 6
 // Explanation: The last word is "joyboy" with length 6.
 
 var lengthOfLastWord = function(s) {
-
+  let reversed = s.split(" ").reverse()
+  console.log(reversed);
+  for (let i = 0; i < reversed.length; i++) {
+    if (reversed[i].length > 0) {
+      return reversed[i].length
+    }
+  }
 };
+
+console.log(lengthOfLastWord(s))
