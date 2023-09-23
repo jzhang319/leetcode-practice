5bbf141cd42c3e125e2a3f13796f0e86e6799923
jzhang319 <jzhang319@gmail.com>
2023-09-23 16:27:28 -0400

    second try

--- 033plusOne.js
+++ 033plusOne.js
@@ -1,6 +1,6 @@
 // Example 1:
 
-let digits = [1, 2, 3];
+// let digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
 // Output: [1,2,4]
 // Explanation: The array represents the integer 123.
 // Incrementing by one gives 123 + 1 = 124.
@@ -14,19 +14,24 @@ let digits = [1, 2, 3];
 // Thus, the result should be [4,3,2,2].
 // Example 3:
 
-// Input: digits = [9]
+Input: digits = [9,9]
 // Output: [1,0]
 // Explanation: The array represents the integer 9.
 // Incrementing by one gives 9 + 1 = 10.
 // Thus, the result should be [1,0].
 
 var plusOne = function (digits) {
-  let combined = digits.join("");
-  let number = Number(combined);
-  number++;
-  // console.log(typeof combined);
-  // console.log(combined);
-  return number.toString().split("");
+  if (digits.length == 1 && digits[digits.length - 1] == 9) {
+    digits.pop()
+    console.log(digits)
+    digits.push(1,0)
+    return(digits)
+  }
+  if (digits.length > 2 && digits[digits.length - 1] == 9) {
+    
+  }
+  digits[digits.length-1]++
+  return digits
 };
 
 console.log(plusOne(digits));
