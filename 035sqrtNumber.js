2c9d3a248a20742195550e8b51d3a091ae32d139
jzhang319 <jzhang319@gmail.com>
2023-09-27 15:21:29 -0400

    i think it works

--- 035sqrtNumber.js
+++ 035sqrtNumber.js
@@ -1,18 +1,27 @@
 // Example 1:
 
-// let x = 4
+// let x = 4;
 // Output: 2
 // Explanation: The square root of 4 is 2, so we return 2.
 // Example 2:
 
-// Input: x = 8
+Input: x = 101
 // Output: 2
 // Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
 
+var mySqrt = function (x) {
+  if (x === 0) return 0;
+  if (x === 1) return 1;
+  for (let i = 2,j = 2; i <= x, j <= x; i++, j++) {
+    // console.log(i)
 
-var mySqrt = function(x) {
-  
+    // console.log(i * j === x);
+    if (i * j === x) return i;
+    if (i * j > x) return i - 1
+  }
 };
+
+console.log(mySqrt(x));
 
 // const x = 2
 // let y = 4
