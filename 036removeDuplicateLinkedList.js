fcadfee15a01f04897280d3ece3970b176bf78aa
jzhang319 <jzhang319@gmail.com>
2023-09-29 13:57:01 -0400

    testing

--- 036removeDuplicateLinkedList.js
+++ 036removeDuplicateLinkedList.js
@@ -10,8 +10,12 @@ var deleteDuplicates = function (head) {
 
   while (cur && cur.next) {
     if (cur.val == cur.next.val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
     }
   }
+  return head;
 };
 
 console.log(deleteDuplicates(head));
