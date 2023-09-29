690dcb2a134665004cf07bb557bf0cf72818a260
jzhang319 <jzhang319@gmail.com>
2023-09-29 12:27:13 -0400

    still working

--- 036removeDuplicateLinkedList.js
+++ 036removeDuplicateLinkedList.js
@@ -1,10 +1,14 @@
 // Example 1:
-// Input: head = [1,1,2]
+let head = [1,1,2]
 // Output: [1,2]
 
 //Example 2:
 // Input: head = [1, 1, 2, 3, 3];
 // Output: [1, 2, 3];
 var deleteDuplicates = function(head) {
-
+  let result = []
+  
+  return result
 };
+
+console.log(deleteDuplicates(head))
