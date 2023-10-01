ad1491233cb92440375eac3ae40a0d1b7e615b01
jzhang319 <jzhang319@gmail.com>
2023-09-30 21:14:41 -0400

    starting 038

---
+++ 038sameTree.js
@@ -0,0 +1,16 @@
+// Example1:
+// Input: (p = [1, 2, 3]), (q = [1, 2, 3]);
+// Output: true;
+
+
+// Example2:
+// Input: (p = [1, 2]), (q = [1, null, 2]);
+// Output: false;
+
+// Example3:
+// Input: (p = [1, 2, 1]), (q = [1, 1, 2]);
+// Output: false;
+
+var isSameTree = function(p, q) {
+
+};
