1636a9c3419c67df240052d07f2c3fcd8da19782
jzhang319 <jzhang319@gmail.com>
2023-10-02 15:02:23 -0400

    working

--- 039romanToInteger.js
+++ 039romanToInteger.js
@@ -1,11 +1,11 @@
 // Example 1:
 
-let s = "III"
+// let s = "III"
 // Output: 3
 // Explanation: III = 3.
 // Example 2:
 
-// Input: s = "LVIII"
+Input: s = "LVIII";
 // Output: 58
 // Explanation: L = 50, V= 5, III = 3.
 // Example 3:
@@ -23,6 +23,16 @@ let s = "III"
 // D             500
 // M             1000
 
-var romanToInt = function(s) {
-  
+var romanToInt = function (s) {
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    let curr = s[i];
+    console.log(s[i]);
+    if (curr == "I") {
+      sum += 1;
+    }
+  }
+  return sum;
 };
+
+console.log(romanToInt(s));
