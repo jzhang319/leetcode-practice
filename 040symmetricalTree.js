01291707afe496aa8425896c01cff6a88699e4a3
jzhang319 <jzhang319@gmail.com>
2023-10-03 14:03:28 -0400

    finished 040

--- 040symmetricalTree.js
+++ 040symmetricalTree.js
@@ -1,11 +1,22 @@
 // Example 1:
-// Input: root = [1,2,2,3,4,4,3]
+let root = [1,2,2,3,4,4,3]
 // Output: true
 
 // Example 2:
 // Input: root = [1, 2, 2, null, 3, null, 3];
 // Output: false;
 
-var isSymmetric = function(root) {
-
+var isSymmetric = function (root) {
+  if (root === null) return true;
+  return compareLeftRight(root.left, root.right);
 };
+
+const compareLeftRight = (left, right) => {
+  if (left === null && right === null) return true;
+  if (left === null && right !== null) return false;
+  if (left !== null && right === null) return false;
+  if (left.val!== right.val) return false;
+  return compareLeftRight(left.left, right.right) && compareLeftRight(left.right, right.left);
+};
+
+console.log(isSymmetric(root))
