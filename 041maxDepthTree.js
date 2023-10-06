d4cd306f99d2c9ea1b55bcf5aaca3822263202dc
jzhang319 <jzhang319@gmail.com>
2023-10-06 11:10:32 -0400

    starting 041

---
+++ 041maxDepthTree.js
@@ -0,0 +1,12 @@
+// Example 1:
+// let root = [3,9,20,null,null,15,7]
+// Output: 3
+// Example 2:
+
+// Input: root = [1,null,2]
+// Output: 2
+
+var maxDepth = function (root) {
+  if (root === null) return 0;
+  
+};
