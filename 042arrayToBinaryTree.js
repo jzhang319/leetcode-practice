7e2eef900d548885d1f07511b74604ccb3521762
jzhang319 <jzhang319@gmail.com>
2023-10-07 18:48:55 -0400

    trying

--- 042arrayToBinaryTree.js
+++ 042arrayToBinaryTree.js
@@ -1,5 +1,5 @@
 // Example 1:
-// let nums = [-10,-3,0,5,9]
+let nums = [-10,-3,0,5,9]
 // Output: [0,-3,9,-10,null,5]
 // Explanation: [0,-10,5,null,-3,null,9] is also accepted:
 
@@ -11,5 +11,12 @@
 
 var sortedArrayToBST = function(nums) {
   let middleNum = Math.floor(nums.length/2)
-  
+  if (!nums.length) return null;
+  let mid = Math.floor(nums.length/2)
+  let root = new TreeNode(nums[mid])
+  root.left = sortedArrayToBST(nums.slice(0, mid))
+  root.right = sortedArrayToBST(nums.slice(mid+1))
+  return root;
 };
+
+console.log(sortedArrayToBST(nums))
