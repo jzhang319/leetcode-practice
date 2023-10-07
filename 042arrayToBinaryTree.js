b928cb7a56990234b68e217bfad052138a865064
jzhang319 <jzhang319@gmail.com>
2023-10-07 18:26:15 -0400

    starting 042

---
+++ 042arrayToBinaryTree.js
@@ -0,0 +1,15 @@
+// Example 1:
+// let nums = [-10,-3,0,5,9]
+// Output: [0,-3,9,-10,null,5]
+// Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+
+// Example 2:
+// let nums = [1,3]
+// Output: [3,1]
+// Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+var sortedArrayToBST = function(nums) {
+  let middleNum = Math.floor(nums.length/2)
+  
+};
