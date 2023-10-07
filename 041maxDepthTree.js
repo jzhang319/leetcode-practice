5a04a67561ea505e6b1ecc966856e899325150e3
jzhang319 <jzhang319@gmail.com>
2023-10-07 18:13:41 -0400

    trying

--- 041maxDepthTree.js
+++ 041maxDepthTree.js
@@ -1,5 +1,5 @@
 // Example 1:
-// let root = [3,9,20,null,null,15,7]
+let root = [3,9,20,null,null,15,7]
 // Output: 3
 // Example 2:
 
@@ -8,5 +8,8 @@
 
 var maxDepth = function (root) {
   if (root === null) return 0;
-  
+  return Math.max(maxDepth(root.left) + 1, maxDepth(root.right)) + 1;
 };
+
+
+console.log(maxDepth(root))
