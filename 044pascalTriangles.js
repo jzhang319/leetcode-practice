3f033a9f47fbdadb28c99d899a5d4d1a088c50c3
jzhang319 <jzhang319@gmail.com>
2023-10-09 11:59:30 -0400

    starting 044

---
+++ 044pascalTriangles.js
@@ -0,0 +1,12 @@
+// Example 1:
+
+// let numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Example 2:
+
+// let numRows = 1
+// Output: [[1]]
+
+var generate = function(numRows) {
+
+};
