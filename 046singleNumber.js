6bf2b4ce2b852897589cfdd17dff25fbbb14cd08
jack <jzhang319@gmail.com>
2023-10-14 16:20:39 -0400

    starting 046

---
+++ 046singleNumber.js
@@ -0,0 +1,16 @@
+// Example 1:
+
+// let nums = [2,2,1]
+// Output: 1
+// Example 2:
+
+// let nums = [4,1,2,1,2]
+// Output: 4
+// Example 3:
+
+// let nums = [1]
+// Output: 1
+
+var singleNumber = function(nums) {
+
+};
