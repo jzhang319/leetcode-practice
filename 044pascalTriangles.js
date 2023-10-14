ece2ff44898a9309140fd41dde7978924182b054
jack <jzhang319@gmail.com>
2023-10-14 16:05:26 -0400

    finished 044

--- 044pascalTriangles.js
+++ 044pascalTriangles.js
@@ -8,25 +8,24 @@ let numRows = 5;
 // Output: [[1]]
 
 var generate = function (numRows) {
-  if(numRows < 1) return [];
-  if(numRows === 1) return [[1]];
-  const triangle = [[1]]
+  if (numRows < 1) return [];
+  if (numRows === 1) return [[1]];
+  const triangle = [[1]];
 
   for (let i = 1; i < numRows; i++) {
     let prevRow = triangle[i - 1];
-    const curRow = []
+    const curRow = [];
 
-    curRow.push(1)
+    curRow.push(1);
 
-    for (let j = prevRow.length; j++){
-      curRow[j] = prevRow[j] + prevRow[j - 1]
+    for (let j = 1; j < prevRow.length; j++) {
+      curRow[j] = prevRow[j] + prevRow[j - 1];
     }
-    curRow.push(1)
+    curRow.push(1);
 
     triangle.push(curRow);
   }
   return triangle;
 };
-
 
 console.log(generate(numRows));
