f78a29bcda373c358acabef641124aee8b8a8689
jack <jzhang319@gmail.com>
2023-10-14 16:12:02 -0400

    starting 045

---
+++ 035pascalTriangles2.js
@@ -0,0 +1,16 @@
+// Example 1:
+
+// let rowIndex = 3
+// Output: [1,3,3,1]
+// Example 2:
+
+// let rowIndex = 0
+// Output: [1]
+// Example 3:
+
+// let rowIndex = 1
+// Output: [1,1]
+
+var getRow = function(rowIndex) {
+  
+};
