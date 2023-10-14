3ee76e807fa73fe376fa0193d01b5bd08c8b8f3e
jack <jzhang319@gmail.com>
2023-10-14 17:22:55 -0400

    finished 046

--- 046singleNumber.js
+++ 046singleNumber.js
@@ -1,10 +1,10 @@
 // Example 1:
 
-let nums = [2,2,1]
+// let nums = [2,2,1]
 // Output: 1
 // Example 2:
 
-// let nums = [4,1,2,1,2]
+let nums = [4,1,2,1,2]
 // Output: 4
 // Example 3:
 
@@ -14,7 +14,18 @@ let nums = [2,2,1]
 var singleNumber = function(nums) {
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    
+    if(!map.has(nums[i])) {
+      map.set(nums[i], 1);
+    } else {
+      map.set(nums[i], map.get(nums[i]) + 1);
+    }
   }
-
+  // console.log(map)
+  for (let i = 0; i < nums.length; i++) {
+    if(map.get(nums[i]) === 1) {
+      return nums[i];
+    }
+  }
 };
+
+console.log(singleNumber(nums));
