ab3dc56ecb8dbacd94c17d13054f2551a7b5c08e
jzhang319 <jzhang319@gmail.com>
2023-10-27 11:37:49 -0400

    starting 047

---
+++ 047excelSheetColumnTitle.js
@@ -0,0 +1,16 @@
+// Example 1:
+
+let columnNumber = 1
+// Output: "A"
+// Example 2:
+
+// let columnNumber = 28
+// Output: "AB"
+// Example 3:
+
+// let columnNumber = 701
+// Output: "ZY"
+
+var convertToTitle = function(columnNumber) {
+
+};
