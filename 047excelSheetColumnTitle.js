17db8a2aade5c74bc8a725d2f910d02233c8b49d
jzhang319 <jzhang319@gmail.com>
2023-10-27 17:30:25 -0400

    res is 1 off from the character

--- 047excelSheetColumnTitle.js
+++ 047excelSheetColumnTitle.js
@@ -1,16 +1,24 @@
 // Example 1:
-
-let columnNumber = 1
+// let columnNumber = 1
 // Output: "A"
-// Example 2:
 
+// Example 2:
 // let columnNumber = 28
 // Output: "AB"
-// Example 3:
 
-// let columnNumber = 701
+// Example 3:
+let columnNumber = 701
 // Output: "ZY"
 
 var convertToTitle = function(columnNumber) {
-
+  console.log(columnNumber);
+  let res = '';
+  while (columnNumber >= 0) {
+    res = String.fromCharCode(columnNumber % 26 + 64) + res;
+    console.log(res, ' <-- res');
+    columnNumber = Math.floor(columnNumber / 26) - 1;
+  }
+  return res
 };
+
+console.log(convertToTitle(columnNumber));
