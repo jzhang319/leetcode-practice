1c4256e0cce83992a66b1a5a8574a1744a3e1dfe
jzhang319 <jzhang319@gmail.com>
2023-10-28 13:00:36 -0400

    finished 048

--- 048convertExcelColumnLetter.js
+++ 048convertExcelColumnLetter.js
@@ -4,16 +4,18 @@
 // Output: 1
 // Example 2:
 
-// let columnTitle = "AB"
+let columnTitle = "AB"
 // Output: 28
 // Example 3:
 
-let columnTitle = "ZY";
+// let columnTitle = "ZY";
 // Output: 701
 
 var titleToNumber = function (columnTitle) {
   // ABC
-  // A --> (0*26) + (65 - 64), B --> (1*26) + (66 - 64), c --> (28*26) + (67 - 64)
+  // A --> (0*26) + (65 - 64)
+  // B --> (1*26) + (66 - 64)
+  // C --> (28*26) + (67 - 64)
 
   let sum = 0;
 
