444363603de3211ba932962b1dad04b68f9e3e30
jzhang319 <jzhang319@gmail.com>
2023-10-28 12:17:51 -0400

    2nd try

--- 047excelSheetColumnTitle.js
+++ 047excelSheetColumnTitle.js
@@ -3,22 +3,55 @@
 // Output: "A"
 
 // Example 2:
-// let columnNumber = 28
+let columnNumber = 28;
 // Output: "AB"
 
 // Example 3:
-let columnNumber = 701
+// let columnNumber = 701;
 // Output: "ZY"
 
-var convertToTitle = function(columnNumber) {
-  console.log(columnNumber);
-  let res = '';
-  while (columnNumber >= 0) {
-    res = String.fromCharCode(columnNumber % 26 + 64) + res;
-    console.log(res, ' <-- res');
-    columnNumber = Math.floor(columnNumber / 26) - 1;
+var convertToTitle = function (columnNumber) {
+  let string = "";
+  let key = [
+    "Z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+  ];
+  if (columnNumber <= 0) {
+    return string;
   }
-  return res
+  while (columnNumber > 0) {
+    let place = columnNumber % 26;
+    string = key[place] + string;
+    columnNumber =
+      place === 0
+        ? Math.floor(columnNumber / 26) - 1
+        : Math.floor(columnNumber / 26);
+  }
+  return string;
 };
 
 console.log(convertToTitle(columnNumber));
