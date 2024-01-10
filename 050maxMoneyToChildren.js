4fa00a1683dcc56d2adb238e6d01ee3a653db8f1
jzhang319 <jzhang319@gmail.com>
2024-01-10 16:57:18 -0500

    starting 050

---
+++ 050maxMoneyToChildren.js
@@ -0,0 +1,21 @@
+// You have to distribute the money according to the following rules:
+
+// All money must be distributed.
+// Everyone must receive at least 1 dollar.
+// Nobody receives 4 dollars.
+// Return the maximum number of children who may receive exactly 8 dollars if you distribute the money according to the aforementioned rules. If there is no way to distribute the money, return -1.
+
+const distMoney = function(money, children) {
+
+};
+
+/* Example 1:
+Input: money = 20, children = 3
+Output: 1
+Explanation:
+The maximum number of children with 8 dollars will be 1. One of the ways to distribute the money is:
+- 8 dollars to the first child.
+- 9 dollars to the second child.
+- 3 dollars to the third child.
+It can be proven that no distribution exists such that number of children getting 8 dollars is greater than 1.
+*/
