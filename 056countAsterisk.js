fe64ac4ba00bc19af7895047cf03b0ec6e4b3adf
jzhang319 <jzhang319@gmail.com>
2024-01-13 19:25:29 -0500

    starting 056

--- 056countAsterisk.js
+++ 056countAsterisk.js
@@ -1,3 +1,16 @@
 var countAsterisks = function (s) {
-  
+    let stack = []
+    let count = 0
+    for (char of s) {
+        if (char === '|') {
+            stack.push(char)
+        } else {
+            if (char === '*' && stack.length % 2 == 0) {
+                count++
+            }
+
+        }
+
+    }
+    return count
 };
